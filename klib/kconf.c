76b35702f79da7fff7058365cd733c667e5d7ac3
Kicer Jiao <joykicer@gmail.com>
2014-04-17 22:31:15 +0800

    add get_string() support

--- klib/kconf.c
+++ klib/kconf.c
@@ -10,16 +10,124 @@
 #undef _KLIB_H_INSIDE_
 
 typedef enum {
-	FIND_KEY_START,
-	FIND_KEY_END,
-	FIND_VAL_START,
-	FIND_VAL_END,
-	PARSE_CONFIG_END,
+	FIND_WORD_START,
+	FIND_WORD_END,
+	PARSE_WORD_END,
 } CONFIG_STATE;
 
-#define is_comment(c) ((c) == '#')
-#define is_sep(c) ((c) == '=')
+typedef struct {
+	int key_start;
+	int key_len;
+	int quote_flag;
+	const char *pbuf;
+} WORD_INFO;
+
+#define SEP_CHAR		'='
+#define COMMENT_CHAR	'#'
+#define is_comment(c) ((c) == COMMENT_CHAR)
+#define is_sep(c) ((c) == SEP_CHAR)
 #define is_quote(c) (((c) == '"') || ((c) == '\''))
+
+
+static WORD_INFO chk_word(const char *buf) {
+	const char * pbuf = buf;
+	CONFIG_STATE state = FIND_WORD_START;
+
+	int key_start = 0;
+	int key_len = -1;
+	int quote_flag = -1;
+	int space_flag = -1;
+
+	while(*pbuf != 0) {
+		switch(state) {
+			case FIND_WORD_START:
+				if(isspace(*pbuf)) {
+				} else if(is_comment(*pbuf)) {
+					/* comment line */
+					state = PARSE_WORD_END;
+				} else if(is_quote(*pbuf)) {
+					quote_flag = *pbuf;
+					char *ptmp = strchr(pbuf+1, quote_flag);
+					if(ptmp != NULL) {
+						key_len = ptmp - pbuf - 1;
+						if(key_len > 0) {
+							key_start = pbuf - buf + 1;
+							state = PARSE_WORD_END;
+							pbuf = ptmp;
+						} else {
+							/* key is "" */
+							key_len = -1;
+							state = PARSE_WORD_END;
+						}
+					} else {
+						/* single-quote key */
+						state = PARSE_WORD_END;
+					}
+				} else {
+					key_start = pbuf - buf;
+					state = FIND_WORD_END;
+				}
+				break;
+
+			case FIND_WORD_END:
+				if(is_sep(*pbuf) || is_comment(*pbuf)) {
+					if(space_flag == -1) {
+						key_len = pbuf - buf - key_start;
+					} else {
+						key_len = space_flag - key_start;
+					}
+					if(key_len <= 0) {
+						/* key is "" */
+						key_len = -1;
+					}
+					state = PARSE_WORD_END;
+				} else if(isspace(*pbuf)) {
+					if(space_flag == -1)
+						space_flag = pbuf - buf;
+				} else {
+					space_flag = -1;
+				}
+				break;
+			default:
+				break;
+		}
+		if(state == PARSE_WORD_END) break;
+		pbuf++;
+	}
+
+	WORD_INFO t;
+	t.key_start = key_start;
+	t.key_len = key_len;
+	t.quote_flag = quote_flag;
+	t.pbuf = pbuf;
+
+	return t;
+}
+
+static const char *parse_key(const char *buf, const char *key) {
+	WORD_INFO t = chk_word(buf);
+
+	if(t.key_len > 0) {
+		if(t.key_len == strlen(key)) {
+			if(strncmp(key, buf+t.key_start, t.key_len) == 0)
+				return t.pbuf;
+		}
+	}
+
+	return NULL;
+}
+
+static int parse_value(const char *buf, char **val) {
+	WORD_INFO t = chk_word(buf);
+
+	if(t.key_len > 0) {
+		*val = malloc(t.key_len+1);
+		*((*val)+t.key_len) = 0;
+		memcpy(*val, buf+t.key_start, t.key_len);
+	}
+
+	return 0;
+}
 
 char* k_get_string(const char *key, const char *file) {
 	FILE *fp = NULL;
@@ -33,83 +141,13 @@ char* k_get_string(const char *key, const char *file) {
 	}
 	
 	while(fgets(buf, sizeof(buf), fp) != NULL) {
-		char *pbuf = buf;
-		CONFIG_STATE state = FIND_KEY_START;
-
-		int key_start = 0;
-		int key_len = -1;
-		int val_start = 0;
-		int val_len = -1;
-		int quote_flag = -1;
-		int space_flag = -1;
-
-		while(*pbuf != 0) {
-			switch(state) {
-				case FIND_KEY_START:
-					if(isspace(*pbuf)) {
-					} else if(is_comment(*pbuf)) {
-						/* comment line */
-						state = PARSE_CONFIG_END;
-					} else if(is_quote(*pbuf)) {
-						quote_flag = *pbuf;
-						char *ptmp = strchr(pbuf+1, quote_flag);
-						if(ptmp != NULL) {
-							key_len = ptmp - pbuf - 1;
-							if(key_len > 0) {
-								key_start = pbuf - buf + 1;
-								state = FIND_VAL_START;
-								pbuf = ptmp;
-							} else {
-								/* key is "" */
-								key_len = -1;
-								state = PARSE_CONFIG_END;
-							}
-						} else {
-							/* single-quote key */
-							state = PARSE_CONFIG_END;
-						}
-					} else {
-						key_start = pbuf - buf;
-						state = FIND_KEY_END;
-					}
-					break;
-				case FIND_KEY_END:
-					if(is_sep(*pbuf) || is_comment(*pbuf)) {
-						if(space_flag == -1) {
-							key_len = pbuf - buf - key_start;
-						} else {
-							key_len = space_flag - key_start;
-						}
-						if(key_len > 0) {
-							state = FIND_VAL_START;
-						} else {
-							/* key is "" */
-							key_len = -1;
-							state = PARSE_CONFIG_END;
-						}
-					} else if(isspace(*pbuf)) {
-						if(space_flag == -1)
-							space_flag = pbuf - buf;
-					} else {
-						space_flag = -1;
-					}
-					break;
-				case FIND_VAL_START:
-					break;
-				case FIND_VAL_END:
-					break;
-				default:
-					break;
+		const char *pbuf = parse_key(buf, key);
+		if(pbuf != NULL) {
+			const char *pstart = strchr(pbuf, SEP_CHAR);
+			if(pstart != NULL) {
+				parse_value(pstart+1, &pval);
 			}
-			if(state == PARSE_CONFIG_END) break;
-			pbuf++;
-		}
-
-		if(key_len > 0) {
-			char tmpbuf[key_len+1];
-			tmpbuf[key_len] = 0;
-			memcpy(tmpbuf, buf+key_start, key_len);
-			printf("%s\tKEY: %d,%d <%s>\n\n", buf, key_start, key_len, tmpbuf);
+			break;
 		}
 	}
 
@@ -150,7 +188,7 @@ int main(int argc, char **argv) {
 		exit(1);
 	}
 
-	printf("Value of <%s> is: %d\n", "ONE ONE", k_get_int("ONE ONE", argv[1]));
+	printf("Value of <%s> is: %d\n", "ONE   ONE", k_get_int("ONE   ONE", argv[1]));
 
 	return 0;
 }
